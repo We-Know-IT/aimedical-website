0ca48527ab1e084a73bb85aad1e668108ce35dfb
EliasFalk <elias.falk@hotmail.com>
2022-12-10 11:55:33 +0100

    add footer - mobile version

---
+++ components/general/footer/index.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import LogoIcon from "../../icons/common/logo";
+
+const contactInformation = {
+  email: "support@aimedtech.org",
+  phone: "+46 721748339",
+  address: {
+    street: "Universitetsvägen 8",
+    city: "Stockholms County",
+    zip: "114 18",
+    country: "SE",
+  },
+};
+
+export default function Footer() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
+  const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const onSubmit = () => {
+    // TODO: Send email
+    console.log(email, message);
+  };
+
+  return (
+    <footer
+      className="flex flex-col bg-c p-16 w-full space-y-20"
+      style={{
+        background:
+          "linear-gradient(105.38deg, rgba(0, 99, 175, 0.8) 0%, rgba(0, 99, 175, 0.5) 100%), #FFFFFF;",
+      }}>
+      <div className="flex flex-col space-y-4">
+        <h2 className="text-2xl text-white font-bold">Send us a message</h2>
+        <span className="bg-white w-32 h-1 rounded-full" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={onEmailChange}
+          value={email}
+          placeholder="Email"
+          className="p-4 rounded-xl"
+        />
+
+        <textarea
+          id="message"
+          name="message"
+          onChange={onMessageChange}
+          value={message}
+          placeholder="Your message"
+          className="p-4 rounded-xl h-40 resize-none"
+        />
+        <button
+          type="submit"
+          onClick={onSubmit}
+          /* TODO: Change hover background to use tailwind config*/
+          className="bg-white rounded-full text-lg px-11 py-3 w-fit shadow-md hover:bg-gray-200">
+          Submit
+        </button>
+      </div>
+      <div className="flex flex-col space-y-24">
+        <div>
+          <div className="flex flex-col space-y-4">
+            <h2 className="text-2xl text-white font-bold">Contacts</h2>
+            <span className="bg-white w-16 h-1 rounded-full" />
+            <p className="text-lg text-white font-bold">
+              {contactInformation.phone}
+            </p>
+            <p className="text-lg text-white">
+              <b>{contactInformation.address.street}</b>
+              <br />
+              {contactInformation.address.city} {contactInformation.address.zip}
+              , {contactInformation.address.country}
+            </p>
+            <p className="text-lg text-white font-bold">
+              {contactInformation.email}
+            </p>
+          </div>
+        </div>
+        <div className="flex  justify-between items-center">
+          <div className="flex flex-col justify-center items-center space-y-4">
+            <LogoIcon w={69} h={69} />
+            <h2 className="text-xl text-white font-bold">
+              AI Medical <br />
+              Technology
+            </h2>
+          </div>
+          <div>
+            <p className="text-white text-sm">
+              Website Privacy policy <br />
+              Stockholm, Sweden <br /> © 2022 All rights reserved
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -13,13 +12,46 @@ module.exports = {
         blue: {
           dark: "rgb(0, 99, 175)",
           light: "rgba(0, 99, 175, 0.5)",
+          100: "rgb(0, 99, 175)",
+          85: "rgba(0,99,175, 0.85)",
+          50: "rgba(0, 99, 175, 0.5)",
         },
         "text-primary": colors.black,
         "text-secondary": colors.white,
         "bg-primary": colors.white,
         "bg-secondary": "rgba(243, 243, 243, 0.502)",
+        "color-on-primary": colors.black,
+        "color-on-secondary": colors.black,
+        "color-on-blue": colors.white,
+        "background-primary": colors.white,
+        "background-secondary": "rgba(243, 243, 243, 0.502)",
+      },
+      fontFamily: {
+        lato: ['"Lato"', "Arial", "Sans-serif"],
+      },
+
+      fontSize: {
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.625rem",
+        "2xl": "1.74rem",
+        "3xl": "2.125rem",
+        "4xl": "2.625rem",
       },
     },
   },
   plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
+
+/*
+Font sizes in design:
+42 / 16 = 2.625REM
+34px/16px = 2.125rem
+28px / 16px = 1.74 rem
+26px / 16px = 1.625rem
+18px / 16 = 1.125 rem
+16/16 = 1 rem
+14px / 16 = 0.875
+*/
