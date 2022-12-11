1f9497a05a38f35168b8805a718212a85903f00f
EliasFalk <elias.falk@hotmail.com>
2022-12-11 14:25:25 +0100

    use tailwind gradient instead of css style

--- components/general/footer/index.tsx
+++ components/general/footer/index.tsx
@@ -32,11 +32,7 @@ export default function Footer() {
   };
 
   return (
-    <footer
-      style={{
-        background:
-          "linear-gradient(105.38deg, rgba(0, 99, 175, 0.8) 0%, rgba(0, 99, 175, 0.5) 100%), #FFFFFF;",
-      }}>
+    <footer className="bg-gradient-to-r from-blue-85 to-blue-50">
       <div className="flex flex-col bg-c py-16 w-full space-y-20 md:flex-row-reverse md:justify-between md:space-y-0 md:container md:mx-auto px-5 ">
         <div className="flex flex-col space-y-4 md:w-1/2">
           <h2 className="text-2xl text-white font-bold">Send us a message</h2>
