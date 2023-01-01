11f1121290b370be1ba82a604383f88c40d623f4
EliasFalk <elias.falk@hotmail.com>
2023-01-01 14:04:21 +0100

    remove unused import

--- components/general/footer/index.tsx
+++ components/general/footer/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import LogoIcon from "../../icons/common/logo";
 import Button from "../button";
-import Image from "next/image";
 import { isValidEmail, isValidMessage } from "../../../utils/validation";
 
 const contactInformation = {
