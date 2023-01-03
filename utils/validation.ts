6f87f98d907221991c9a317cedb7fe19fcbddfbb
EliasFalk <elias.falk@hotmail.com>
2023-01-03 13:32:36 +0100

    change email regex

--- utils/validation.ts
+++ utils/validation.ts
@@ -2,9 +2,10 @@ function isValidEmail(email: string) {
   if (!email) {
     return false;
   }
-  const validEmailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+  /* Regex from https://emailregex.com/ under the title Javascript */
+  const validEmailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!validEmailRegex.test(email)) {
     return false;
   }
